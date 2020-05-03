7ec66ed68f1ac68739a1ab22f13bdbc0444df7db
sixro <me@sixro.net>
2020-05-03 13:06:01 +0200

    Added source files as required by course

---
+++ week1/percolation/src/main/java/PercolationStats.java
@@ -0,0 +1,31 @@
+public class PercolationStats {
+    // perform independent trials on an n-by-n grid
+    public PercolationStats(int n, int trials) {
+
+    }
+
+    // sample mean of percolation threshold
+    public double mean() {
+        return -1d;
+    }
+
+    // sample standard deviation of percolation threshold
+    public double stddev() {
+        return -1d;
+    }
+
+    // low endpoint of 95% confidence interval
+    public double confidenceLo() {
+        return -1d;
+    }
+
+    // high endpoint of 95% confidence interval
+    public double confidenceHi() {
+        return -1d;
+    }
+
+    // test client (see below)
+    public static void main(String[] args) {
+        
+    }
+}
