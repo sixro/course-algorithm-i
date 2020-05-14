8edd3f04c012703ba66329387e14a7dca38d01b7
sixro <me@sixro.net>
2020-05-14 21:11:22 +0200

    Added base objects required by the assignment; disabled spotsbug 'cause objects provided have issues and I cannot update them apparently

---
+++ week3/collinear-points/src/main/java/BruteCollinearPoints.java
@@ -0,0 +1,18 @@
+public class BruteCollinearPoints {
+    // finds all line segments containing 4 points
+    public BruteCollinearPoints(Point[] points) {
+        // FIXME impl
+    }
+
+    // the number of line segments
+    public int numberOfSegments() {
+        // FIXME impl
+        return 0;
+    }
+
+    // the line segments
+    public LineSegment[] segments() {
+        // FIXME impl
+        return null;
+    }
+}
---
+++ week3/collinear-points/src/main/java/FastCollinearPoints.java
@@ -0,0 +1,19 @@
+public class FastCollinearPoints {
+    // finds all line segments containing 4 or more points
+    public FastCollinearPoints(Point[] points) {
+        // FIXME impl
+    }
+
+    // the number of line segments
+    public int numberOfSegments() {
+        // FIXME impl
+        return 0;
+    }
+
+    // the line segments
+    public LineSegment[] segments() {
+        // FIXME impl
+        return null;
+    }
+
+}
