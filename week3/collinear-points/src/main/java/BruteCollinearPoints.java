c1a5222fa727647099745eaebe26df115c1aa968
sixro <me@sixro.net>
2020-05-17 09:46:06 +0200

    Added tests

--- week3/collinear-points/src/main/java/BruteCollinearPoints.java
+++ week3/collinear-points/src/main/java/BruteCollinearPoints.java
@@ -1,4 +1,7 @@
 public class BruteCollinearPoints {
+
+    private final LineSegment[] segments;
+
     // finds all line segments containing 4 points
     public BruteCollinearPoints(Point[] points) {
         if (points == null)
@@ -6,18 +9,25 @@ public class BruteCollinearPoints {
         for (Point p: points)
             if (p == null)
                 throw new IllegalArgumentException("no point null please");
-        // FIXME impl
+        if (points[0].compareTo(points[1]) == 0 || points[0].compareTo(points[2]) == 0 || points[0].compareTo(points[3]) == 0 || points[1].compareTo(points[2]) == 0 || points[1].compareTo(points[3]) == 0 || points[2].compareTo(points[3]) == 0)
+            throw new IllegalArgumentException("found duplicates");
+
+        segments = new LineSegment[4];
+        double slopePQ = points[0].slopeTo(points[1]);
+        double slopeQR = points[1].slopeTo(points[2]);
+        double slopeRS = points[2].slopeTo(points[3]);
+        double slopeSP = points[2].slopeTo(points[3]);
+        
+        if (slopeRS == slopeQR)
     }
 
     // the number of line segments
     public int numberOfSegments() {
-        // FIXME impl
-        return 0;
+        return segments.length;
     }
 
     // the line segments
     public LineSegment[] segments() {
-        // FIXME impl
-        return null;
+        return segments;
     }
 }
--- week3/collinear-points/src/test/java/BruteCollinearPointsTest.java
+++ week3/collinear-points/src/test/java/BruteCollinearPointsTest.java
@@ -1,3 +1,4 @@
+import org.junit.Ignore;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -18,6 +19,20 @@ public class BruteCollinearPointsTest {
     public void ctor_with_duplicates() {
         new BruteCollinearPoints(new Point[]{ new Point(1, 1),
             new Point(1, 1) });
+    }
+
+    @Test
+    public void different_segments() {
+        Point p = new Point(1, 1);
+        Point q = new Point(2, 1);
+        Point r = new Point(2, 2);
+        Point s = new Point(1, 2);
+        BruteCollinearPoints bcp = new BruteCollinearPoints(new Point[]{p, q, r, s});
+        assertEquals(4, bcp.numberOfSegments());
+        assertEquals(new LineSegment(p, q), bcp.segments()[0]);
+        assertEquals(new LineSegment(q, r), bcp.segments()[1]);
+        assertEquals(new LineSegment(r, s), bcp.segments()[2]);
+        assertEquals(new LineSegment(s, p), bcp.segments()[3]);
     }
 
 }
