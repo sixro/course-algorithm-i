3f3f07bf80515d456efec8778ecfb8034fe1e40f
sixro <me@sixro.net>
2020-05-17 09:56:05 +0200

    Added FastCollinear  impl

--- week3/collinear-points/src/main/java/BruteCollinearPoints.java
+++ week3/collinear-points/src/main/java/BruteCollinearPoints.java
@@ -1,33 +1,77 @@
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
 public class BruteCollinearPoints {
 
     private final LineSegment[] segments;
 
-    // finds all line segments containing 4 points
     public BruteCollinearPoints(Point[] points) {
-        if (points == null)
-            throw new IllegalArgumentException("points are are required");
-        for (Point p: points)
-            if (p == null)
-                throw new IllegalArgumentException("no point null please");
-        if (points[0].compareTo(points[1]) == 0 || points[0].compareTo(points[2]) == 0 || points[0].compareTo(points[3]) == 0 || points[1].compareTo(points[2]) == 0 || points[1].compareTo(points[3]) == 0 || points[2].compareTo(points[3]) == 0)
-            throw new IllegalArgumentException("found duplicates");
 
-        segments = new LineSegment[4];
-        double slopePQ = points[0].slopeTo(points[1]);
-        double slopeQR = points[1].slopeTo(points[2]);
-        double slopeRS = points[2].slopeTo(points[3]);
-        double slopeSP = points[2].slopeTo(points[3]);
-        
-        if (slopeRS == slopeQR)
+        checkNull(points);
+        Point[] sortedPoints = points.clone();
+        Arrays.sort(sortedPoints);
+        checkDuplicate(sortedPoints);
+
+        final int N = points.length;
+        List<LineSegment> list = new LinkedList<>();
+
+        for (int a = 0; a < N - 3; a++) {
+            Point ptA = sortedPoints[a];
+
+            for (int b = a + 1; b < N - 2; b++) {
+                Point ptB = sortedPoints[b];
+                double slopeAB = ptA.slopeTo(ptB);
+
+                for (int c = b + 1; c < N - 1; c++) {
+                    Point ptC = sortedPoints[c];
+                    double slopeAC = ptA.slopeTo(ptC);
+                    if (slopeAB == slopeAC) {
+
+                        for (int d = c + 1; d < N; d++) {
+                            Point ptD = sortedPoints[d];
+                            double slopeAD = ptA.slopeTo(ptD);
+                            if (slopeAB == slopeAD) {
+                                list.add(new LineSegment(ptA, ptD));
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        segments = list.toArray(new LineSegment[0]);
     }
 
-    // the number of line segments
+    private void checkNull(Point[] points) {
+        if (points == null) {
+            throw new IllegalArgumentException("points are required");
+        }
+        for (Point p : points) {
+            if (p == null) {
+                throw new IllegalArgumentException("all points required");
+            }
+        }
+    }
+
+    private void checkDuplicate(Point[] points) {
+        for (int i = 0; i < points.length - 1; i++) {
+            if (points[i].compareTo(points[i + 1]) == 0) {
+                throw new IllegalArgumentException("Duplicate(s) found.");
+            }
+        }
+    }
+
+    /**
+     * The number of line segments.
+     */
     public int numberOfSegments() {
         return segments.length;
     }
 
-    // the line segments
+    /**
+     * The line segments.
+     */
     public LineSegment[] segments() {
-        return segments;
+        return segments.clone();
     }
 }
--- week3/collinear-points/src/main/java/FastCollinearPoints.java
+++ week3/collinear-points/src/main/java/FastCollinearPoints.java
@@ -1,19 +1,76 @@
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
 public class FastCollinearPoints {
-    // finds all line segments containing 4 or more points
+    private final LineSegment[] segments;
+
     public FastCollinearPoints(Point[] points) {
-        // FIXME impl
+
+        checkNull(points);
+        Point[] sortedPoints = points.clone();
+        Arrays.sort(sortedPoints);
+        checkDuplicate(sortedPoints);
+
+        final int N = points.length;
+        final List<LineSegment> maxLineSegments = new LinkedList<>();
+
+        for (int i = 0; i < N; i++) {
+
+            Point p = sortedPoints[i];
+            Point[] pointsBySlope = sortedPoints.clone();
+            Arrays.sort(pointsBySlope, p.slopeOrder());
+
+            int x = 1;
+            while (x < N) {
+
+                LinkedList<Point> candidates = new LinkedList<>();
+                final double SLOPE_REF = p.slopeTo(pointsBySlope[x]);
+                do {
+                    candidates.add(pointsBySlope[x++]);
+                } while (x < N && p.slopeTo(pointsBySlope[x]) == SLOPE_REF);
+
+                if (candidates.size() >= 3
+                    && p.compareTo(candidates.peek()) < 0) {
+                    Point min = p;
+                    Point max = candidates.removeLast();
+                    maxLineSegments.add(new LineSegment(min, max));
+                }
+            }
+        }
+        segments = maxLineSegments.toArray(new LineSegment[0]);
     }
 
-    // the number of line segments
+    private void checkNull(Point[] points) {
+        if (points == null) {
+            throw new IllegalArgumentException("points are required");
+        }
+        for (Point p : points) {
+            if (p == null) {
+                throw new IllegalArgumentException("all points required");
+            }
+        }
+    }
+
+    private void checkDuplicate(Point[] points) {
+        for (int i = 0; i < points.length - 1; i++) {
+            if (points[i].compareTo(points[i + 1]) == 0) {
+                throw new IllegalArgumentException("Duplicate(s) found.");
+            }
+        }
+    }
+
+    /**
+     * The number of line segments.
+     */
     public int numberOfSegments() {
-        // FIXME impl
-        return 0;
+        return segments.length;
     }
 
-    // the line segments
+    /**
+     * The line segments.
+     */
     public LineSegment[] segments() {
-        // FIXME impl
-        return null;
+        return segments.clone();
     }
-
 }
